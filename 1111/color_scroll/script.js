26c1cb3215a7790f5af4406a7dd3260896d6ab9e
mb5iz <kato.miu5iz@gmail.com>
2025-12-23 14:23:36 +0900

    1223

--- 1111/color_scroll/script.js
+++ 1111/color_scroll/script.js
@@ -9,7 +9,7 @@ const settingColors = [
 
 //5.documentの縦の長さ取得
 const fullHeight = document.documentElement.scrollHeight;
-console.log(fullheight);
+console.log(fullHeight);
 
 
 //2.オブジェクトの色をbodyの背景色にする
@@ -27,63 +27,77 @@ document.body.style.backgroundColor =
 
 //3.4色制作して、配列にする
 
+//10.関数の定義
+function colorchange(num) {
+    document.body.style.backgroundColor =
+        'rgb(' +
+        settingColors[num].r +
+        ',' +
+        settingColors[num].g +
+        ',' +
+        settingColors[num].b +
+        ')';
+}
 
 //4.スクロールするたびにイベント
 window.addEventListener('scroll', function () {
     console.log('スクロール');
-})
 
-const scroly
+    const scrollY = window.screenY;
+    console.log(screenY);
 
+    //7.スクロールを4分割
+    if (scrollY < fullHeight / 4) {
+        document.body.style.backgroundColor =
+            'rgb(' +
+            settingColors[0].r +
+            ',' +
+            settingColors[0].g +
+            ',' +
+            settingColors[0].b +
+            ')';
+    } else if (scrollY < fullHeight / 2) {
+        document.body.style.backgroundColor =
+            'rgb(' +
+            settingColors[1].r +
+            ',' +
+            settingColors[1].g +
+            ',' +
+            settingColors[1].b +
+            ')';
+    } else if (scrollY < (fullHeight * 3) / 4) {
+        document.body.style.backgroundColor =
+            'rgb(' +
+            settingColors[2].r +
+            ',' +
+            settingColors[2].g +
+            ',' +
+            settingColors[2].b +
+            ')';
+    } else {
+        document.body.style.backgroundColor =
+            'rgb(' +
+            settingColors[3].r +
+            ',' +
+            settingColors[3].g +
+            ',' +
+            settingColors[3].b +
+            ')';
+    }
 
-
-
-
+});
 
 //6.1 / 4進んだら色が変わるようにする
 
-
-//7.スクロールを4分割
-if (scrollY < fullHeight / 4) {
-    document.body.style.backgroundColor =
-        'rgb(' +
-        settingColors[0].r +
-        ',' +
-        settingColors[0].g +
-        ',' +
-        settingColors[0].b +
-        ')';
-} else if (scrollY < fullHeight / 2) {
-    document.body.style.backgroundColor =
-        'rgb(' +
-        settingColors[1].r +
-        ',' +
-        settingColors[1].g +
-        ',' +
-        settingColors[1].b +
-        ')';
-} else if (scrollY < (fullHeight * 3) / 4) {
-    document.body.style.backgroundColor =
-        'rgb(' +
-        settingColors[2].r +
-        ',' +
-        settingColors[2].g +
-        ',' +
-        settingColors[2].b +
-        ')';
-} else {
-    document.body.style.backgroundColor =
-        'rgb(' +
-        settingColors[3].r +
-        ',' +
-        settingColors[3].g +
-        ',' +
-        settingColors[3].b +
-        ')';
-}
-
-
 //8.スクロール量を計算
 //9.変数scrollableを4分割
-//10.関数の定義
+
+window.addEventListener('scroll', function () {
+    const button = document.querySelector('.movetotop');
+    if (this.window.scrollY >= 200) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+});
 //11.関数の呼び出し（実行）の実引数
